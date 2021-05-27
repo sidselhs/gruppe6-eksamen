21a0a880bc60c398ae98bdb9ba32f8adb62183bb
sidselhs <72500459+sidselhs@users.noreply.github.com>
2021-05-27 13:17:27 +0200

    Add files via upload

---
+++ astra-child (2)/footer.php
@@ -0,0 +1,61 @@
+<!php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+<?php 
+	astra_content_after();
+		
+	astra_footer_before();
+		
+	astra_footer();
+		
+	astra_footer_after(); 
+?>
+	</div><!-- #page -->
+
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
+<button onclick="topFunction()" id="myBtn" title="Go to top">Til Toppen</button>
+<script>
+    alert("Velkommen til! Vi vil understrege, at dette er en prototype til et eksamensprojekt. Derfor kan du ikke bestille plakater mm. på denne hjemmeside");  // display string message
+    
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+</script>
+	</body>
+</html>
